3a626fd1e1f26c6d72f4b6cf4a67f51499b618d6
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2025-04-20 23:19:51 +0800

    make dirt shadow gender-dynamic

--- My project/Assets/Resources/Scripts/Controllers/CharacterMaskController.cs
+++ My project/Assets/Resources/Scripts/Controllers/CharacterMaskController.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using UnityEngine.UI; // Needed for RawImage
 
 public class CharacterMaskController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Texture2D maleMask;
+    public Texture2D femaleMask;
+    public Material baseMaterial;
+
+    private Material characterMaterial;
+    private RawImage rawImage;
+
     void Start()
     {
-        
+        rawImage = GetComponent<RawImage>();
+        if (rawImage == null)
+        {
+            Debug.LogError("RawImage component not found on this GameObject.");
+            return;
+        }
+
+        if (baseMaterial != null)
+        {
+            // Duplicate the material
+            characterMaterial = new Material(baseMaterial);
+            rawImage.material = characterMaterial;
+
+            ApplyMaskBasedOnGender();
+        }
+        else
+        {
+            Debug.LogWarning("Base material is not assigned in the inspector!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ApplyMaskBasedOnGender()
     {
-        
+        Debug.Log("ApplyMaskBasedOnGender() called");
+
+        if (characterMaterial == null) return;
+
+        Texture2D chosenMask = (GameDataManager.heroGender == "M") ? maleMask : femaleMask;
+
+        if (chosenMask != null)
+        {
+            characterMaterial.SetTexture("_MaskTex", chosenMask);
+            Debug.Log("Assigned mask: " + chosenMask.name);
+        }
+        else
+        {
+            Debug.LogWarning("Chosen mask texture is null!");
+        }
     }
 }
